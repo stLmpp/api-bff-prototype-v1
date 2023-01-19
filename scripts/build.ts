d46d9fd4f54611b9482638e20d8e9ead9dc57506
stLmpp <gui.stlmpp@gmail.com>
2023-01-19 13:47:50 -0300

    - update rimraf

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,21 +1,9 @@
 import { build } from 'esbuild';
 import fastGlob from 'fast-glob';
-import rimraf from 'rimraf';
-
-async function rimrafAsync(path: string): Promise<void> {
-  return new Promise((resolve, reject) => {
-    rimraf(path, (error) => {
-      if (error) {
-        reject(error);
-        return;
-      }
-      resolve();
-    });
-  });
-}
+import { rimraf } from 'rimraf';
 
 async function main(): Promise<void> {
-  await rimrafAsync('dist');
+  await rimraf('dist');
   const files = await fastGlob(['src/**/*.{ts,mts}', 'app/**/*.{ts,mts}']);
   await build({
     entryPoints: files,
